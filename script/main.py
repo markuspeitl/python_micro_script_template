300e4c172a83058942cc8af0b137bc225578e76b
Markus Peitl <office@markuspeitl.com>
2024-08-02 21:28:27 +0200

    Added some more argparse options

--- script/main.py
+++ script/main.py
@@ -4,13 +4,14 @@ from typing import Any
 
 
 def my_program_function(config: dict[str, Any] = {}):
-    print('Calling ')
+    print('Hello world from python micro script template')
 
 
 def add_script_parsing_options(parser: argparse.ArgumentParser):
     parser.add_argument('source_path', help="")
-    parser.add_argument('target_path', help="")
     parser.add_argument('-id', '--input_dir', help="")
+    parser.add_argument('-m', '-multi', '--multi_args', nargs="+", help="")
+    parser.add_argument('-d', '--default', help="", default='default value', choices=['default value', 'other value'])
 
 
 def main(parser: argparse.ArgumentParser | None = None):
